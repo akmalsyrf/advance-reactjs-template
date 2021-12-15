0aa4498f0defcc5f759b5878cdcb34451e055342
akmalsyrf <dev.akmal69@gmail.com>
2021-12-15 12:26:33 +0700

    update

--- src/components/DetailAbout.js
+++ src/components/DetailAbout.js
@@ -1,5 +1,20 @@
 // import package for property types
+import PropTypes from "prop-types";
 
 // create component here
+export default function DetailAbout(props) {
+  return (
+    <>
+      <h1>Title : {props.title}</h1>
+      <p>Summary : {props.summary}</p>
+      <p>Total Guest : {props.total}</p>
+    </>
+  );
+}
 
 // define property type for this component here
+DetailAbout.propTypes = {
+  title: PropTypes.string.isRequired,
+  summary: PropTypes.string,
+  total: PropTypes.number, //error muncul di console
+};
--- src/pages/About.js
+++ src/pages/About.js
@@ -1,4 +1,5 @@
 import { Container } from "react-bootstrap";
+import DetailAbout from "../components/DetailAbout";
 
 // import component
 
@@ -7,16 +8,12 @@ function About() {
     <Container className="text-center">
       <p className="h1">About Us</p>
       <p>
-        Maecenas bibendum vel tortor non congue. Maecenas at sodales turpis, id
-        maximus odio. Donec aliquet elementum mauris, vel semper tortor ultrices
-        a. Cras pulvinar tristique diam, vel lobortis lacus cursus quis. Morbi
-        ante nibh, laoreet tincidunt fringilla non, fringilla ac ligula. Duis
-        libero orci, rhoncus quis ultricies et, euismod sit amet purus. In
-        mollis lectus nunc, vel tempus lorem lobortis id. Nullam maximus elit
-        quis nibh gravida, ut hendrerit felis fringilla. Proin at aliquam
-        tortor.
+        Maecenas bibendum vel tortor non congue. Maecenas at sodales turpis, id maximus odio. Donec aliquet elementum mauris, vel semper tortor ultrices a. Cras pulvinar tristique diam, vel lobortis lacus cursus quis. Morbi ante nibh,
+        laoreet tincidunt fringilla non, fringilla ac ligula. Duis libero orci, rhoncus quis ultricies et, euismod sit amet purus. In mollis lectus nunc, vel tempus lorem lobortis id. Nullam maximus elit quis nibh gravida, ut hendrerit
+        felis fringilla. Proin at aliquam tortor.
       </p>
       {/* code here */}
+      <DetailAbout title="Dumbways" summary="hola" total={50} />
     </Container>
   );
 }
